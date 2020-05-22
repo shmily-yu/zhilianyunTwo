fa76c1e65a535e886404b38b7a8c92b33798fee6
yumufeng <yuqiutao1995@hotmail.com>
2020-05-22 16:31:59 +0800

    同上剪切方法

--- src/tools/index.js
+++ src/tools/index.js
@@ -60,3 +60,47 @@ export const reg = {
     phone: /^1([0-9][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,
 };
 
+
+//压缩图片
+export const compressImage = function compressImage(file) {
+    if (file.size > 1024 * 30) {
+        return new Promise((resolve, reject) => {
+            // 获取图片（加载图片是为了获取图片的宽高）
+            const img = new Image();
+            img.src = window.URL.createObjectURL(file);
+            img.onerror = error => reject(error);
+            img.onload = () => {
+                // 画布宽高
+                const canvasWidth =
+                    document.documentElement.clientWidth * window.devicePixelRatio;
+                const canvasHeight =
+                    document.documentElement.clientHeight * window.devicePixelRatio;
+
+                // 计算缩放因子
+                // 这里我取水平和垂直方向缩放因子较大的作为缩放因子，这样可以保证图片内容全部可见
+                const scaleX = canvasWidth / img.width;
+                const scaleY = canvasHeight / img.height;
+                // const scale = Math.min(scaleX, scaleY);//选这种可能会有黑色区域，但可以保证图片内容全部可见
+                const scale = Math.max(scaleX, scaleY); //选这种图片内容可能不完整，但不会有黑色区域，图片是充满的
+
+                // 将原始图片按缩放因子缩放后，绘制到画布上
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                canvas.width = canvasWidth;
+                canvas.height = canvasHeight;
+                const imageWidth = img.width * scale;
+                const imageHeight = img.height * scale;
+                const dx = (canvasWidth - imageWidth) / 2;
+                const dy = (canvasHeight - imageHeight) / 2;
+                ctx.drawImage(img, dx, dy, imageWidth, imageHeight);
+                // 导出新图片
+                // 指定图片 MIME 类型为 'image/jpeg', 通过 quality 控制导出的图片质量，进行实现图片的压缩
+                const quality = 0.1;
+                canvas.toBlob(file => resolve(file), "image/jpeg", quality); //压缩返回bolb文件
+            };
+        });
+    } else {
+        return file; //不用压缩则直接返回file文件
+    }
+}
+
